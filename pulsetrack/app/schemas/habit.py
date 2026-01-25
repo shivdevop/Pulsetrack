522cac18c91d77ea134bda0aea215b352f3b59b1
Shivam Agarwalla <shivamagarwalla05@gmail.com>
2026-01-25 22:23:04 +0530

    added schemas related to habit routes requests and responses

---
+++ pulsetrack/app/schemas/habit.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional,Literal
+from datetime import datetime 
+
+class HabitBase(BaseModel):
+    name: str
+    frequency_type: Literal["daily","weekly","custom"]
+    target_count: int 
+
+class HabitCreate(HabitBase):
+    pass
+
+class HabitUpdate(BaseModel):
+    name: Optional[str]=None
+    frequency_type: Optional[Literal["daily","weekly","custom"]]=None
+    target_count: Optional[int]=None 
+
+
+class HabitResponse(HabitBase):
+    id: int
+    created_at:datetime 
+
+
+    class Config:
+        from_attributes= True
